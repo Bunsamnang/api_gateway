289f54f7dd179086a7b84ffcbff81dbef93fee85
Bunsamnang <bunsamnangk@gmail.com>
2025-05-29 15:50:41 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import httpProxy from "http-proxy";
+
+const app = express();
+
+//USE PROXY SERVER TO REDIRECT THE INCOMMING REQUEST
+const proxy = httpProxy.createProxyServer();
+
+//REDIRECT TO THE STUDENT MICROSERVICE
+app.use("/student", (req, res) => {
+  console.log("INSIDE API GATEWAY STUDENT ROUTE (cloud mini proj folder)");
+  proxy.web(req, res, { target: "http://localhost:5001" });
+});
+
+//REDIRECT TO THE TEACHER MICROSERVICE
+app.use("/teacher", (req, res) => {
+  console.log("INSIDE API GATEWAY TEACHER ROUTE (cloud_native folder)");
+  proxy.web(req, res, { target: "http://localhost:5002" });
+});
+
+app.listen(4000, () => {
+  console.log("API Gateway Service is running on PORT NO : ", 4000);
+});
