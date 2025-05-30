ee9c006b897144e31981dca614c592bada90d2ae
Khut Bunsamnang <138737983+Bunsamnang@users.noreply.github.com>
2025-05-30 16:26:02 +0700

    change localhost to ec2 public ip

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ const proxy = httpProxy.createProxyServer();
 //REDIRECT TO THE STUDENT MICROSERVICE
 app.use("/student", (req, res) => {
   console.log("INSIDE API GATEWAY STUDENT ROUTE");
-  proxy.web(req, res, { target: "http://localhost:5001" });
+  proxy.web(req, res, { target: "http://13.220.210.1:5001" });
 });
 
 //REDIRECT TO THE TEACHER MICROSERVICE
 app.use("/teacher", (req, res) => {
   console.log("INSIDE API GATEWAY TEACHER ROUTE");
-  proxy.web(req, res, { target: "http://localhost:5002" });
+  proxy.web(req, res, { target: "http://3.86.100.196:5002" });
 });
 
 app.listen(4000, () => {
